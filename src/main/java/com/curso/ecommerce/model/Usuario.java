c89757502ff1d7226caa0c7e0b24cb5d41062b21
LuisGM-303 <lgarciamalagan@gmail.com>
2023-04-07 01:17:22 -0600

    modified:   src/main/java/com/curso/ecommerce/model/Usuario.java

--- src/main/java/com/curso/ecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -1,7 +1,17 @@
 package com.curso.ecommerce.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String username;
